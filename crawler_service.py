3d525d8c12bbe5dd9c59409f2b5eb33b2e62eb49
Superchipper <SuperChipper@users.noreply.github.com>
2024-04-09 14:41:56 +0100

    crawler_upd

--- crawler_service.py
+++ crawler_service.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 @app.route('/crawl', methods=['GET'])
 def crawl_data():
     # TODO: Implement your crawler logic here and fetch data
-    
+    cookies = json.load(open('cookies.json'))
     res = requests.get("https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&host_mid=37507923&timezone_offset=-60&platform=web&features=itemOpusStyle,listOnlyfans"
                        ,headers={'User-Agent':'Mozilla/5.0'},
-                       cookies={})
+                       cookies=cookies) 
     item1 = res.json()['data']['items']
     res = requests.get("https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&host_mid=3493265644980448&timezone_offset=-60",headers={'User-Agent':'Mozilla/5.0'},
-                       cookies={}) 
+                       cookies=cookies) 
     item2 = res.json()['data']['items']
     def process(item,fname):
         try:
@@ -25,9 +25,12 @@ def crawl_data():
             ts = json.loads('{"pub_ts": 0}')
         if item[1]['type'] == "DYNAMIC_TYPE_DRAW":
             if ts['pub_ts'] != item[1]['modules']['module_author']['pub_ts']:
-                text = item[1]['modules']['module_dynamic']['major']['opus']['summary']['text']
-                pictures = [url['url'] for url in item[1]['modules']['module_dynamic']['major']['opus']['pics']]
-                
+                if item[1]['modules']['module_dynamic']['major']['type'] == 'MAJOR_TYPE_OPUS':
+                    text = item[1]['modules']['module_dynamic']['major']['opus']['summary']['text']#item[1]['modules']['module_dynamic']['desc'][1]['orig_text']
+                    pictures = [url['url'] for url in item[1]['modules']['module_dynamic']['major']['opus']['pics']]
+                else:
+                    text = item[1]['modules']['module_dynamic']['desc']['text']
+                    pictures = [url['src'] for url in item[1]['modules']['module_dynamic']['major']['draw']['items']]
                 ts['pub_ts'] = item[1]['modules']['module_author']['pub_ts']
                 data = {
                     "update": True,
@@ -66,5 +69,5 @@ def crawl_data():
 
 
 if __name__ == "__main__":
-    
+    #crawl_data()
     app.run(port=8081)
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import requests
 
 import json
 from flask import Flask, jsonify
-res = requests.get("https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&host_mid=37507923&timezone_offset=-60&platform=web&features=itemOpusStyle,listOnlyfans",headers={'User-Agent':'Mozilla/5.0'})
+res = requests.get("https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&host_mid=37507923&timezone_offset=-60&platform=web&features=itemOpusStyle,listOnlyfans",headers={'User-Agent':'Mozilla/5.0'},
+                   cookies={'SESSDATA':'aa8486f3%2C1717499470%2Ca96b0%2Ac1CjDsinf5F4ETKi7izwuZquWLXJexReMNpBSQJ_uxqJHkbV3Eg5P_VWo9r18EQ_hYkYsSVk9lUzh5NkVpU2pFMGZDYzRGLWhTTC1lb0dwQ19TYVhMZkxsVFA2T19HSWtJdng0bFZYR3NqQng0UF9WX3NETjc3d295R0ZzVE1SYndGNHUwbGVlZDF3IIEC'})
 item = res.json()
 print(item)
