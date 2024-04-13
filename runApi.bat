49fbad49d608d7f9d6951a71589fad1f19c614a7
Superchipper <SuperChipper@users.noreply.github.com>
2024-04-13 01:45:00 +0100

    cookie_update

--- runApi.bat
+++ runApi.bat
@@ -1,2 +1,2 @@
-start cmd /k "conda activate GLM6B && python openai_api.py"
+start cmd /k "conda activate GLM6B && python openai_api_demo/api_server.py"
 start cmd /k "conda activate && python crawler_service.py"
